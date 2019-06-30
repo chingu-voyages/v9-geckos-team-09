8811796bbc117458a2db6cbe90c33d649e6491e9
Samir70home <sbetmouni@talktalk.net>
2019-06-30 06:34:31 +0100

    Updates

---
+++ precache-manifest.9df10b665f877781ee9fc49e218773e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd07b91d9ab06227d55c7fcbbc7913e",
+    "url": "/v9-geckos-team-09/index.html"
+  },
+  {
+    "revision": "477eabdb93e03e1635fe",
+    "url": "/v9-geckos-team-09/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5c18b1f1f74aac015360",
+    "url": "/v9-geckos-team-09/static/css/main.4994d628.chunk.css"
+  },
+  {
+    "revision": "477eabdb93e03e1635fe",
+    "url": "/v9-geckos-team-09/static/js/2.6b731214.chunk.js"
+  },
+  {
+    "revision": "5c18b1f1f74aac015360",
+    "url": "/v9-geckos-team-09/static/js/main.4de72967.chunk.js"
+  },
+  {
+    "revision": "412e015ea8377f72127e",
+    "url": "/v9-geckos-team-09/static/js/runtime~main.644353f9.js"
+  }
+]);
